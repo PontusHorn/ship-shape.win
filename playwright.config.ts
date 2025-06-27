104436cf0d1534ed882a301c3628c9c6f72e6021
Pontus Horn <pontus.horn@finch3d.com>
2025-06-27 22:27:07 +0200

    Run e2e tests in more browsers
    Not mobile for now, as it's broken. Will need to fix the layout there
    first.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,33 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices, ViewportSize } from '@playwright/test';
+
+const LARGE_DESKTOP_VIEWPORT: ViewportSize = { width: 1920, height: 1080 };
 
 export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'e2e'
+	testDir: 'e2e',
+
+	projects: [
+		// Small computer screen
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		},
+
+		// Larger computer screen
+		{
+			name: 'chromium (large)',
+			use: { ...devices['Desktop Chrome'], viewport: LARGE_DESKTOP_VIEWPORT }
+		}
+	]
 });
